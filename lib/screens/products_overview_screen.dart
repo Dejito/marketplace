27660382ce446eb6054790b2381a2e83b5de45b8
Deji <Deerealboy@gmail.com>
2023-10-09 23:47:23 +0100

    project set up

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductsOVerviewScreen extends StatelessWidget {
+  const ProductsOVerviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ti danandan"),
+      ),
+      body: Center(child: Text("Ti dandan"),),
+    );
+  }
+}
